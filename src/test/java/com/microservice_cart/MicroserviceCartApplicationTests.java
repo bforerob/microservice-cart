1018c52a4a0d6c4ab0fc626807905c031edcddba
bforerob <bforerob@unal.edu.co>
2024-10-07 08:16:02 -0500

    repo init

---
+++ src/test/java/com/microservice_cart/MicroserviceCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice_cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
